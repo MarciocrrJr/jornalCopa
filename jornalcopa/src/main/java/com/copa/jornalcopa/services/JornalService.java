e9c7fb31bc09755e2b463b2e7a8610daee785f9c
MarciocrrJr <marciocjcrr2@gmail.com>
2026-06-14 20:29:57 -0300

    feat(service): adiciona metodos para salvar, atualizar e deletar jornal

--- jornalcopa/src/main/java/com/copa/jornalcopa/services/JornalService.java
+++ jornalcopa/src/main/java/com/copa/jornalcopa/services/JornalService.java
@@ -1,27 +1,52 @@
 package com.copa.jornalcopa.services;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.copa.jornalcopa.entities.Jornal;
 import com.copa.jornalcopa.repositories.JornalRepository;
-
 import jakarta.persistence.EntityNotFoundException; 
 
 @Service
 public class JornalService {
 
-    @Autowired
-    private JornalRepository jornalRepository;
+    private final JornalRepository repository;
+
+    public JornalService(JornalRepository repository) {
+        this.repository = repository;
+    }
 
     public List<Jornal> findAll() {
-        return jornalRepository.findAll();
+        return repository.findAll();
     }
 
     public Jornal findById(long id) {
-        return jornalRepository.findById(id)
-                            .orElseThrow(() -> new EntityNotFoundException());
+        return repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Jornal não encontrado"));
+    }
+
+    public void deleteById(Long id) {
+        if (repository.existsById(id)) {
+            repository.deleteById(id);
+        } else {
+            throw new EntityNotFoundException("Jornal não encontrado");
+        }
+    }
+
+    public Jornal save(Jornal jornal) {
+        return repository.save(jornal);
+    }
+
+    public void update(Jornal jornal, Long id) {
+        Jornal j = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Jornal não encontrado"));
+
+        j.setTitulo(jornal.getTitulo());
+        j.setSubtitulo(jornal.getSubtitulo());
+        j.setTexto(jornal.getTexto());
+        j.setAutor(jornal.getAutor());
+        j.setData(jornal.getData());
+
+        repository.save(j);
     }
 }

d19d4775e5e61f5a2cd92da344ff29c539940dc0
MarciocrrJr <marciocjcrr2@gmail.com>
2026-06-14 18:39:20 -0300

    Criação do projeto utilizando java e springboot

---
+++ jornalcopa/src/main/java/com/copa/jornalcopa/JornalcopaApplication.java
@@ -0,0 +1,13 @@
+package com.copa.jornalcopa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JornalcopaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JornalcopaApplication.class, args);
+	}
+
+}
---
+++ jornalcopa/src/test/java/com/copa/jornalcopa/JornalcopaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.copa.jornalcopa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JornalcopaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

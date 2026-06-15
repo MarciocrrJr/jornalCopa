e320c29b428f0e94e57a3c62f834b2b62bac641e
MarciocrrJr <marciocjcrr2@gmail.com>
2026-06-15 20:34:34 -0300

    feat(web): adiciona o CrossOrigin para fazer o contato do front com o back

--- jornalcopa/src/main/java/com/copa/jornalcopa/controllers/JornalController.java
+++ jornalcopa/src/main/java/com/copa/jornalcopa/controllers/JornalController.java
@@ -16,6 +16,7 @@ import com.copa.jornalcopa.services.JornalService;
 
 import jakarta.validation.Valid;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/jornal")
+@CrossOrigin(origins = "http://localhost:4200")
 public class JornalController {
 
     private final JornalService service;
